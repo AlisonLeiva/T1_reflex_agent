3468d9cf53cdfb2251f526a8c5037d1cad0f61ee
AlisonLeiva <alisonleiva24@gmail.com>
2021-08-08 00:24:40 -0600

    Se agrego estilo

--- Tarea1_201700378/reflex_agent.js
+++ Tarea1_201700378/reflex_agent.js
@@ -1,56 +1,82 @@
-function reflex_agent(location, state) {
-  if (state == "DIRTY") return "CLEAN";
-  else if (location == "A") return "RIGHT";
+function reflex_agent(location) {
+  if (location == "A") return "RIGHT";
   else if (location == "B") return "LEFT";
 }
 
-function changeImg(state, action) {
-  if (state[0] == "A") {
+function addLog(location, action) {
+  document.getElementById(`log`).innerHTML += `<br><FONT><b>LOCATION:</b> ${location} | <b>ACTION:</b> ${action}</FONT>`;
+}
+
+function changeImg(states) {
+  if (states[2] == "A") {
     document.getElementById("aspiradoraL").src = "aspiradora.png";
-    document.getElementById("aspiradoraR").src =
-      "https://fondosmil.com/fondo/17538.jpg";
-  } else if (state[0] == "B") {
+    document.getElementById("aspiradoraR").src = "https://fondosmil.com/fondo/17538.jpg";
+    
+  } else if (states[2] == "B") {
     document.getElementById("aspiradoraR").src = "aspiradora.png";
-    document.getElementById("aspiradoraL").src =
-      "https://fondosmil.com/fondo/17538.jpg";
+    document.getElementById("aspiradoraL").src = "https://fondosmil.com/fondo/17538.jpg";
+  }
+  document.getElementById("trashL").src = (states[0]=="DIRTY") ? "basura.png" : "check.png";
+  document.getElementById("trashR").src = (states[1]=="DIRTY") ? "basura.png" : "check.png";
+}
+
+function test() {
+  var estado;
+  var location = states[2];
+  var state = location== "A" ? states[0] : states[1];
+  var action_result = reflex_agent(location);
+
+  if (location   === "A") {
+    //IZQUIERDA
+    estado = state === "DIRTY"
+        ? cleanThis(1, 0, 3, 1, location)
+        : changeRoom(5, 7, location, action_result, 1);
+  } else {
+    //DERECHA
+    estado =  state === "DIRTY"
+        ? cleanThis(0, 1, 6, 2, location)
+        : changeRoom(4, 8, location, action_result, 0);
   }
 
-  if (action == "CLEAN" && state[0] == "A") {
-    document.getElementById("trashL").src = "check.png";
-  }
-
-  if (action == "CLEAN" && state[0] == "B") {
-    document.getElementById("trashR").src = "check.png";
+  
+  document.getElementById(`estado${estado}`).innerHTML = (count[estado - 1] += 1);
+  dirty(location);
+  changeImg(states)
+  if (Math.min(...count) < 2) {
+    setTimeout(function () { test(); }, 1000);
   }
 }
 
-function test(states) {
-  var location = states[0];
-  var state = states[0] == "A" ? states[1] : states[2];
-  var action_result = reflex_agent(location, state);
-
-  changeImg(states, action_result);
-
-  document.getElementById("log").innerHTML += "<br>Location: "
-    .concat(location)
-    .concat(" | Action: ")
-    .concat(action_result);
-
-  if (action_result == "CLEAN") {
-    if (location == "A") states[1] = "CLEAN";
-    else if (location == "B") states[2] = "CLEAN";
-  } else if (action_result == "RIGHT") states[0] = "B";
-  else if (action_result == "LEFT") states[0] = "A";
-  setTimeout(function () {
-    test(states);
-  }, 2000);
+function cleanThis(index, index2, status1, status2, place) {
+  addLog(place,'CLEAN');
+  states[index2] = "CLEAN";
+  return states[index] === "CLEAN" ? status1 : status2;
 }
 
-const printInitial = () => {
-  document.getElementById("trashL").src = "basura.png";
-  document.getElementById("trashR").src = "basura.png";
-};
+function changeRoom(status1, status2, place, lado, index) {
+  addLog( place, `MOVE TO ${lado}`);
+  states[2] = states[2] === "A" ? "B" : "A";
+  return states[index] === "DIRTY" ? status1 : status2;
+}
 
-var states = ["A", "DIRTY", "DIRTY"];
-printInitial();
-test(states);
+function dirty(location) {
+  
+  var probability = Math.floor(Math.random() * 11);
+
+  if (probability < 2) {
+    states[0] = "DIRTY";
+    addLog('A','DIRTY');
+  }
+  if (probability < 4 && 2 <= probability) {
+    states[1] = "DIRTY";
+    addLog('B','DIRTY');
+  }
+  if (probability == 10) {
+    states = ["DIRTY", "DIRTY", location];
+    addLog('A & B','DIRTY');
+  }
+}
+
+var states = ["DIRTY", "DIRTY", "A"];
+var count = [0, 0, 0, 0, 0, 0, 0, 0];
+test();
